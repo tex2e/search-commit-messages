c4764e6603c4e47bda1d7ae0dc07c696200ab3a4
Mako <mnfeconicu41@gmail.com>
2016-05-12 21:10:16 +0900

    make it readable

--- html/js/main.js
+++ html/js/main.js
@@ -158,7 +158,7 @@ console.time("load file");
 var getDatabase = jQuery.get("database/commit-message")
   .done(function (data) {
     console.timeEnd("load file");
-    Array.prototype.push.apply(comments, data.split("\n"));
+    comments = data.split("\n");
   })
   .fail(function (e) {
     $('#react').text('error: ' + e.statusText);
